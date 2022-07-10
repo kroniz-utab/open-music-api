a112c01d4717d2270bec94f1af32b81a4588e710
kroniz-utab <adehf12@gmail.com>
2022-07-10 19:56:19 +0700

    openmusic v2.0 start adding table for new feature

--- migrations/1657436583549_create-albums-table.js
+++ migrations/1657436583549_create-albums-table.js
@@ -7,8 +7,6 @@ exports.up = (pgm) => {
     id: { type: 'VARCHAR(50)', primaryKey: true },
     name: { type: 'TEXT', notNull: true },
     year: { type: 'INT', notNull: true },
-    created_at: { type: 'TEXT', notNull: true },
-    updated_at: { type: 'TEXT', notNull: true },
   });
 };
 
